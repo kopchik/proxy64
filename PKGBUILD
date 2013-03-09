b5773bbbdc8fb0783b6bc52a44f3e96ebb4ca8d6
Alexandre Kandalintsev <spam@messir.net>
2013-03-09 21:14:54 +0100

    added build scripts

---
+++ PKGBUILD
@@ -0,0 +1,10 @@
+pkgname='proxy64'
+pkgver=`date +%G%m%d`
+# pkgver () {
+#   cd "$srcdir/$_gitname"
+#   echo $(git describe --always | sed 's/-/./g')
+# }
+pkgrel=1
+
+arch=('any')
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='proxy64',
+    version='1.0',
+    py_modules = ['proxy64'],
+    license='WTFPL',
+    long_description=open('README.md').read(),
+)

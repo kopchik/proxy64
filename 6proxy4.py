df510e8fcfc6093b7abbb6b5882c5e5106dc44e9
Alexandre Kandalintsev <spam@messir.net>
2013-03-09 20:45:47 +0100

    added resolver

--- 6proxy4.py
+++ 6proxy4.py
@@ -2,15 +2,45 @@
 
 from collections import namedtuple
 import threading
+import ipaddress
 import socket
 import select
 import syslog
+import random
+
 
 Kb = 1024*1024
 Mb = 1024*Kb
 
+
 Addr = namedtuple('Addr', ['ip', 'port'])
 Addr.__str__  = lambda self: "%s:%s" % (self.ip, self.port)
+
+
+def isaddr(addr):
+  try:
+    return ipaddress.ip_address(addr)
+  except:
+    pass
+  return False
+
+def resolve(addr, v4only=False, v6only=False):
+  proto = socket.SOL_TCP
+  family = 0
+  if isaddr(addr):
+    return addr
+
+  assert not (v4only and v6only),
+    "either v4only or v6nly"
+  if v4only:
+    family == socket.AF_INET
+  elif v6only:
+    fimily == socket.AF_INET6
+  r = socket.getaddrinfo(addr, 6666, family=family, proto=proto)
+  return random.choice(r)[4][0]
+
+
+
 class Log:
   def __init__(self, name):
     self.name = name
@@ -85,6 +115,7 @@ class Proxy(threading.Thread):
       self.log.info("connection from {}".format(addr))
       tun = Tun(conn, self.dst, self.buf)
       tun.start()
+
 
 if __name__ == '__main__':
   p = Proxy(src=Addr('127.0.0.1', 6666), dst=Addr('messir.net', 80))
